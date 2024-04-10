25078f645363ea5edae27d7d5d7d3e8644d33d9c
rish93 <rish93@gmail.com>
2024-04-10 17:27:27 +0530

    initial commit

---
+++ src/main/java/com/health/devicesevent/DeviceseventApplication.java
@@ -0,0 +1,13 @@
+package com.health.devicesevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceseventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceseventApplication.class, args);
+	}
+
+}
